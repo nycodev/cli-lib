ea3fa8827f461ea1ce5889d8103ba9eb88b71773
Nycolas Borowski <nycolas_borowski@hotmail.com>
2022-08-16 21:08:33 -0400

    update

--- cli.js
+++ cli.js
@@ -7,7 +7,7 @@ const caminho = process.argv;
 async function processaTexto(caminhoDeArquivo) {
     const resultado = await pegaArquivo(caminhoDeArquivo[2]);
     if (caminho[3] === 'validar') {
-        console.log(chalk.yellow('Links validados'), validaURLs(resultado));
+        console.log(chalk.yellow('Links validados'), await validaURLs(resultado));
     } else {
         console.log(chalk.yellow('Lista de links'), resultado);
     };
--- http-validacao.js
+++ http-validacao.js
@@ -1,5 +1,21 @@
-function validaURLs(arrayLinks) {
-return arrayLinks;
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+async function checaStatus(arrayURLs) {
+    const arrayStatus = await Promise.all(arrayURLs.map(async url => {
+        const res = await fetch(url);
+        return res.status;
+    }))
+    return arrayStatus;
+};
+
+function geraArrayDeURLs(arrayLinks) {
+    return arrayLinks.map(objetoLink => Object.values(objetoLink).join());
+}
+
+async function validaURLs(arrayLinks) {
+    const links = geraArrayDeURLs(arrayLinks);
+    const statusLinks = await checaStatus(links);
+    return statusLinks;
 };
 
 module.exports = validaURLs;
