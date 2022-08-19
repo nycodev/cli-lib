19c4ef7a109b541d7aa0da95e80251e37003b7f5
Nycolas Borowski <nycolas_borowski@hotmail.com>
2022-08-19 17:57:55 -0400

    update

--- http-validacao.js
+++ http-validacao.js
@@ -1,21 +1,36 @@
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+
+function manejaErros(erro) {
+    throw new Error(erro.message);
+}
 
 async function checaStatus(arrayURLs) {
-    const arrayStatus = await Promise.all(arrayURLs.map(async url => {
-        const res = await fetch(url);
-        return res.status;
-    }))
-    return arrayStatus;
+    try {
+        const arrayStatus = await Promise
+            .all(arrayURLs
+                .map(async url => {
+                    const res = await fetch(url);
+                    return res.status;
+                }))
+        return arrayStatus;
+    } catch (erro) {
+        manejaErros(error);
+    }
 };
 
 function geraArrayDeURLs(arrayLinks) {
-    return arrayLinks.map(objetoLink => Object.values(objetoLink).join());
+    return arrayLinks
+        .map(objetoLink => Object
+            .values(objetoLink).join());
 }
 
 async function validaURLs(arrayLinks) {
     const links = geraArrayDeURLs(arrayLinks);
     const statusLinks = await checaStatus(links);
-    return statusLinks;
+    const resultados = arrayLinks.map((objeto, index) => ({
+        ...objeto, status: statusLinks[index]
+    }));
+    return resultados;
 };
 
 module.exports = validaURLs;
